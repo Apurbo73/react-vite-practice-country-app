4749db9d5b0e8ef69e8cdc2fd26515c9b2fe364a
Apurbo73 <apurbodebnath895@gmail.com>
2023-05-29 13:51:58 +0600

    data fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 const url = "https://restcountries.com/v3.1/all";
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [countries, setCountries] = useState([]);
+  const fetchData = async url => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setCountries(data);
+      setIsLoading(false);
+      setError(null);
+
+      console.log(countries);
+    } catch (error) {
+      setIsLoading(false);
+      setError(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(url);
+  }, []);
   return (
     <div>
       <h3>React Country App...</h3>
